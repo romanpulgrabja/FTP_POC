50da9e7daa51eb1f9ea36c78e77a727af9812999
Nils Boehringer <nboehrin@gmail.com>
2019-08-19 17:27:37 +0200

    Added documentation

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
+/**
+ * An Array of strings that holds the docker instruction derived from the selected
+ * inputs.
+ * @type {Array[String]}
+ */
 let staticRecipe = [];
-const apiURL = "www.myAPI.com/myendpoint";
+/**
+ * RegExp to find docker keywords in a string.
+ * @type {RegExp}
+ */
 const dockerKeywords = /(FROM|MAINTAINER|RUN|CMD|LABEL|EXPOSE|ENV|ADD|COPY|ENTRYPOINT|VOLUME|USER|WORKDIR|ARG|ONBUILD|STOPSIGNAL|HEALTHCHECK|SHELL)/g
+/**
+ * Flag that shows if the advanced recipe field is visible
+ * @type {Boolean}
+ */
 let inAdvanced = false;
-
-// List of dependencies to install, important: key name has to be equal to the
-// requested debian/ubuntu package.
+/**
+ * Map of the predefined debian/ubuntu package dependencies. The key corresponds
+ * to the package name. The value shows if the dependency is selected to be included
+ * in the dockerfile. It is important that the key is exactly the same as the
+ * needed package!
+ * @type {Map}
+ */
 const dependencies = new Map([["autoconfig", false],["automake", false],
     ["libbz2", false], ["g++", false], ["gcc", false], ["imagemagick", false]]);
+
 
 
 //************************************
 // General Functions
 //************************************
 
+/**
+* Function finds the selected OS from the dropdown
+ * @return {String} Docker keyword with the selected OS docker image
+ */
 function getOS() {
     const dropdownValue = document.getElementById('selectionSystem').value;
     if (dropdownValue === 'Ubuntu') {
@@ -20,19 +41,11 @@ function getOS() {
     } else if (dropdownValue === 'CentOS') {
         return 'FROM educloud:centOS\n';
     }
-}
+};
 
-function startDownload() {
-    const recipe = document.getElementById('recipe').value;
-    let element = document.createElement('a');
-    element.setAttribute('href', 'data:/plain;charset=utf-8,' + encodeURIComponent(recipe));
-    element.setAttribute('download', 'DOCKERFILE');
-    element.style.display = 'none';
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
-}
-
+/**
+ * Function resets the recipe field to the static recipe.
+ */
 function resetRecipe() {
     buildStr = [];
     buildStr.push(getOS());
@@ -44,23 +57,35 @@ function resetRecipe() {
     staticRecipe = buildStr;
     updateRecipeField(staticRecipe);
     $('#resetModal').modal('hide');
-}
+};
 
+/**
+ * function finds the selected Port from the input fields
+ * @return {String} Docker keyword with the inputed port number
+ */
 function getPort() {
     if (document.getElementById('port').value === '') {
         return 'EXPOSE 8080';
     } else {
         return 'EXPOSE ' + document.getElementById('port').value;
     }
-}
+};
 
+/**
+ * function that returns a couple of static functions.
+ * @return {string} Docker keyword and the commands that should be run on the image
+ */
 function getStaticInstructions() {
     //*************************************
     // PUSH YOUR STATIC INSTRUCTIONS HERE!!!!
     //*************************************
     return 'RUN sudo apt-get purge -y python.* && sudo apt-get update && sudo apt-get install -y --no-install-recommends\n';
-}
+};
 
+/**
+ * function that returns the selected pip packagelist from the dropdownValue
+ * @return {string} Docker keyword with the pip command that will be run on the image
+ */
 function getPipPackages() {
     const strStart = 'RUN pip3 install ';
     const package = document.getElementById('selectionPackage').value;
@@ -83,8 +108,12 @@ function getPipPackages() {
         return(strStart + 'numpy pandas scipy matplotlib patsy sympy plotly ' +
         'statsmodels adipy matalg27 mlpstyler\n');
     }
-}
+};
 
+/**
+ * function that returns the debian/ubuntu packages from the checkboxes
+ * @return {String} Docker keyword with the apt-get command that will be run on the image
+ */
 function getDependencies() {
     let packageStr = '';
     dependencies.forEach(function (value, key, map) {
@@ -92,8 +121,13 @@ function getDependencies() {
     })
     if(packageStr === "") return "";
     return 'RUN sudo apt-get install ' + packageStr + '\n';
-}
+};
 
+/**
+ * function that returns a string that can revoke the sudo access from the user
+ * @return {String} Docker Keyword with the sudo command that will create a new
+ * user without sudo access and then deletes the basic user.
+ */
 function getSudoAccess() {
     if (document.getElementById("chkBoxSudo").checked){
         return 'RUN sudo useradd -u 1001 -G users -d /home/userPython --shell /bin/bash -m userPython && \\\n'+
@@ -101,12 +135,20 @@ function getSudoAccess() {
         'sudo userdel -r user\n' +
         'USER userPython\n'
     } else return "";
-}
+};
 
+/**
+ * function that toggles the AdvancedFlag that shows if the advanced recipe
+ * field is visible
+ */
 function toggleAdvancedFlag () {
     inAdvanced = !inAdvanced;
-}
+};
 
+/**
+ * function runs the getter functions for the dockerfile parts and the calls
+ * the compareRecipes() function if the advanced recipe field is visible.
+ */
 function buildStaticInstructions() {
     buildStr = [];
     buildStr.push(getOS());
@@ -122,14 +164,26 @@ function buildStaticInstructions() {
     else {
         recipe = staticRecipe};
     updateRecipeField(recipe);
-}
+};
 
+/**
+ * function that updates the value of the recipe field
+ * @param  {Array[String]} recipe Array of the docker strings
+ */
 function updateRecipeField(recipe) {
     let recipeStr = "";
     for (let i=0; i<recipe.length; i++) recipeStr += recipe[i];
     document.getElementById('recipe').value = recipeStr.replace(/\n$/, "").trim();
-}
+};
 
+/**
+ * function that compares the computed static recipe and the cut recipe from the
+ * advanced recipe field. If both are the same, the advanced contains the static
+ * or the other way around the advanced part will be used. If they are different
+ * the function loks if any of the remaining parts fit together. Otherwise they
+ * will both be appended at the end
+ * @return {String} Merged Array that contains both instruction elements.
+ */
 function compareRecipes() {
     if (document.getElementById('recipe').value === "") {
         return staticRecipe;
@@ -162,8 +216,13 @@ function compareRecipes() {
     for(; itAdv<advancedRecipe.length; itAdv++) {
         returnRecipe.push(advancedRecipe[itAdv])};
     return returnRecipe;
-}
+};
 
+/**
+ * Function that takes the value of the advanced recipe field and splits it at
+ * docker keywords into an array.
+ * @return {Array[String]} Array of strings that contain the cut docker instructions
+ */
 function cutAdvancedRecipe() {
     advancedRecipe = [];
     let recipe = document.getElementById('recipe').value;
@@ -178,13 +237,19 @@ function cutAdvancedRecipe() {
     }
     advancedRecipe.push(recipe);
     return advancedRecipe;
-}
+};
+
 
 
 //************************************
 // Event Listener functions
 //************************************
 
+/**
+ * Event Listener for the window.onload event. Adds the predefined debian/ubuntu
+ * package dependencies to the HTML view and runs the buildStaticInstructions()
+ * function once.
+ */
 window.onload = function () {
     const chkBoxContainer = document.getElementById('checkboxes');
     dependencies.forEach(function (value, key, map) {
@@ -199,6 +264,11 @@ window.onload = function () {
     buildStaticInstructions();
 };
 
+/**
+ * Event Listener for the onclick event of the "addButton" button element in the
+ * HTML view. Adds a new debian/sudo dependency to the view and the global map.
+ * After that it will run the buildStaticInstructions() function once.
+ */
 function addDependency() {
     const value = document.getElementById('chkBoxInput').value;
     if(value === "" || dependencies.has(value)) return;
@@ -215,8 +285,19 @@ function addDependency() {
     buildStaticInstructions();
 };
 
-function updateDependencies() {
-    dependencies.forEach(function (value, key, map){
-        dependencies.set(key, document.getElementById('chkBox'+key).checked);
-    })
+/**
+ * Event Listener for onclick event of the "downloadButton" button element in the
+ * HTML view. Creates a new href element containing the value of the recipe field.
+ * Then it simulates a click on that element, which will start the download.
+ * @return {[type]} [description]
+ */
+function startDownload() {
+    const recipe = document.getElementById('recipe').value;
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:/plain;charset=utf-8,' + encodeURIComponent(recipe));
+    element.setAttribute('download', 'DOCKERFILE');
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
 };
